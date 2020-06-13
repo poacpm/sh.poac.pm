e87318b86d5c8822ffe429305f41e11495e95426
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-06-13 21:08:25 +0900

    Update poac-setup.sh

--- poac-setup.sh
+++ poac-setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-set -u
+# Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+# file at the top-level directory of this distribution and at
+# http://rust-lang.org/COPYRIGHT.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
 
+set -u
 
 usage() {
     cat 1>&2 <<EOF
@@ -62,7 +71,6 @@ parse_args() {
     done
 }
 
-# Inspired by https://sh.rustup.rs
 get_architecture() {
     local _ostype="$(uname -s)"
     local _cputype="$(uname -m)"
@@ -215,15 +223,6 @@ get_architecture() {
     echo -n "$_arch"
 }
 
-# Copyright 2016 The Rust Project Developers. See the COPYRIGHT
-# file at the top-level directory of this distribution and at
-# http://rust-lang.org/COPYRIGHT.
-#
-# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-# option. This file may not be copied, modified, or distributed
-# except according to those terms.
 say() {
     echo "poac-setup: $@"
 }
